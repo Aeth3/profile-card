f6700c4d33ebd6096e05d5d6005dd927ea4ec2d0
AJ Damalan <bradams163@gmail.com>
2024-01-06 12:09:34 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const profile = {
+  avatar: "images/avatar.jpg",
+  name: "Ama Junriel Damalan",
+  intro:
+    "Adaptable person with knowledge of gathering, transforming, and arranging data for analysis to aid in making decisions. Excellent knowledge and skills in SQL, spreadsheets, Tableau, and other platforms needed for efficient data analysis. solid organizational and communication skills. analytical abilities. Just finished the challenging, practical Google Data Analytics Certificate program, which covers every aspect of the data analysis process.",
+  skillList: [
+    { skill: "HTML/CSS", level: "Advance", color: "red" },
+    { skill: "Flutter", level: "Intermediate", color: "blue" },
+    { skill: "Dart", level: "Intermediate", color: "green" },
+    { skill: "Python", level: "Advance", color: "yellow" },
+    { skill: "R", level: "Advance", color: "pink" },
+    { skill: "JavaScript", level: "Beginner", color: "aqua" },
+    { skill: "React", level: "Beginner", color: "purple" },
+  ],
+};
+function App() {
+  return (
+    <body className="body">
+      <div className="card">
+        <Avatar avatar={profile.avatar} />
+        <div className="data">
+          <Intro profile={profile} />
+
+          <SkillList skills={profile.skillList} />
+        </div>
+      </div>
+    </body>
+  );
+}
+
+function Avatar({ avatar }) {
+  // eslint-disable-next-line jsx-a11y/alt-text
+  return <img className="avatar" src={avatar} />;
+}
+
+function Intro({ profile }) {
+  return (
+    <div>
+      <h1 className="h1">{profile.name}</h1>
+      <p>{profile.intro}</p>
+    </div>
+  );
+}
+
+function SkillList({ skills }) {
+  return (
+    <div className="skill-list">
+      {skills.map((element) => (
+        <Skill skill={element} />
+      ))}
+    </div>
+  );
+}
+function Skill({ skill }) {
+  var level;
+  if (skill.level === "Advance") {
+    level = "💪";
+  } else if (skill.level === "Intermediate") {
+    level = "👍";
+  } else {
+    level = "👶";
+  }
+  return (
+    <div className="skill" style={{ backgroundColor: skill.color }}>
+      <span>{skill.skill}</span>
+      <span>{level}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
